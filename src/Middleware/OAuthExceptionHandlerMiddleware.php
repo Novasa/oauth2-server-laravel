de41662b24e5effa9c3f89720b02a9abdaf80a6f
Michael Lundbøl <michael.lundboel@gmail.com>
2017-05-05 14:32:42 +0200

    Use master Oauth Middleware
    In order to simplify the Oauth flow use the new middleware

--- src/Middleware/OAuthExceptionHandlerMiddleware.php
+++ src/Middleware/OAuthExceptionHandlerMiddleware.php
@@ -15,18 +15,20 @@ class OAuthExceptionHandlerMiddleware implements Middleware
     public function handle($request, Closure $next)
     {
         try {
+            $response = $next($request);
+            // Was an exception thrown? If so and available catch in our middleware
+            if (isset($response->exception) && $response->exception) {
+                throw $response->exception;
+            }
 
-            return $next($request);
-
+            return $response;
         } catch (OAuthException $e) {
+            $data = [
+                'error' => $e->errorType,
+                'error_description' => $e->getMessage(),
+            ];
 
-            return new JsonResponse([
-                    'error'             => $e->errorType,
-                    'error_description' => $e->getMessage()
-                ],
-                $e->httpStatusCode,
-                $e->getHttpHeaders()
-            );
+            return new JsonResponse($data, $e->httpStatusCode, $e->getHttpHeaders());
         }
     }
 }
